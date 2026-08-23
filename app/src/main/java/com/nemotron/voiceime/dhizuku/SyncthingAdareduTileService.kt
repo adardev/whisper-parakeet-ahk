1d895007a3b6f9188a1e78e0b8bc0cf9db22344f
adardev <adardev@users.noreply.github.com>
2026-08-22 23:08:03 -0600

    rename: Sync Adaredu → Syncthing

--- app/src/main/java/com/nemotron/voiceime/dhizuku/SyncthingAdareduTileService.kt
+++ app/src/main/java/com/nemotron/voiceime/dhizuku/SyncthingAdareduTileService.kt
@@ -5,6 +5,6 @@ import com.nemotron.voiceime.R
 class SyncthingAdareduTileService : AppFreezeTileService() {
     override val targetPackage: String = "com.github.catfriend1.syncthingforl"
     override val targetPackages: List<String> = listOf(targetPackage)
-    override val tileLabel: String = "Syncthing"
+    override val tileLabel: String = "Sincthing"
     override val tileIconRes: Int = R.drawable.ic_syncthing_tile
 }
